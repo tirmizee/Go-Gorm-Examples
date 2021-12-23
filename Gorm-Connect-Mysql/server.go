7d0d591a9cb8b0e9ca41ef4e3205ca705607b047
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-12-24 00:47:16 +0700

    revise code

--- Gorm-Connect-Mysql/server.go
+++ Gorm-Connect-Mysql/server.go
@@ -8,13 +8,19 @@ import (
 	"gorm.io/gorm"
 )
 
+type Product struct {
+	gorm.Model
+	Code   string
+	Price  uint
+	Create time.Time
+}
+
 func main() {
-
 	initGormV1()
-
 }
 
 func initGormV1() {
+
 	var (
 		dsn = "user:pass@tcp(0.0.0.0:3306)/example?charset=utf8mb4&parseTime=True&loc=Local"
 	)
@@ -24,19 +30,30 @@ func initGormV1() {
 		panic(err)
 	}
 
-	sqlDB, err := db.DB()
+	if sqlDB, err := db.DB(); err == nil {
+		// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+		sqlDB.SetMaxIdleConns(10)
 
-	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
-	sqlDB.SetMaxIdleConns(10)
+		// SetMaxOpenConns sets the maximum number of open connections to the database.
+		sqlDB.SetMaxOpenConns(100)
 
-	// SetMaxOpenConns sets the maximum number of open connections to the database.
-	sqlDB.SetMaxOpenConns(100)
-
-	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
-	sqlDB.SetConnMaxLifetime(time.Hour)
+		// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+		sqlDB.SetConnMaxLifetime(time.Hour)
+	}
 
 	fmt.Println(*db.Config)
 	fmt.Println(db.ConnPool)
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "D42", Price: 100, Create: time.Now()})
+
+	// Read
+	var product Product
+	db.First(&product, 1)                 // find product with integer primary key
+	db.First(&product, "code = ?", "D42") // find product with code D42
 
 }
 
@@ -45,30 +62,48 @@ func initGormV2() {
 		dsn = "user:pass@tcp(0.0.0.0:3306)/example?charset=utf8mb4&parseTime=True&loc=Local"
 	)
 
-	// _, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	db, err := gorm.Open(mysql.New(mysql.Config{
+	mysqlConfig := mysql.Config{
 		DSN:                       dsn,   // data source name
 		DefaultStringSize:         256,   // default size for string fields
 		DisableDatetimePrecision:  true,  // disable datetime precision, which not supported before MySQL 5.6
 		DontSupportRenameIndex:    true,  // drop & create when rename index, rename index not supported before MySQL 5.7, MariaDB
 		DontSupportRenameColumn:   true,  // `change` when rename column, rename column not supported before MySQL 8, MariaDB
 		SkipInitializeWithVersion: false, // auto configure based on currently MySQL version
-	}), &gorm.Config{})
+	}
+
+	gormConfig := gorm.Config{
+		SkipDefaultTransaction: true,
+		DryRun:                 false,
+		PrepareStmt:            true,
+	}
+
+	db, err := gorm.Open(mysql.New(mysqlConfig), &gormConfig)
 	if err != nil {
 		panic(err)
 	}
 
-	sqlDB, err := db.DB()
+	if sqlDB, err := db.DB(); err == nil {
+		// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+		sqlDB.SetMaxIdleConns(10)
 
-	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
-	sqlDB.SetMaxIdleConns(10)
+		// SetMaxOpenConns sets the maximum number of open connections to the database.
+		sqlDB.SetMaxOpenConns(100)
 
-	// SetMaxOpenConns sets the maximum number of open connections to the database.
-	sqlDB.SetMaxOpenConns(100)
-
-	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
-	sqlDB.SetConnMaxLifetime(time.Hour)
+		// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+		sqlDB.SetConnMaxLifetime(time.Hour)
+	}
 
 	fmt.Println(*db.Config)
 	fmt.Println(db.ConnPool)
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "D42", Price: 100, Create: time.Now()})
+
+	// Read
+	var product Product
+	db.First(&product, 1)                 // find product with integer primary key
+	db.First(&product, "code = ?", "D42") // find product with code D42
 }
